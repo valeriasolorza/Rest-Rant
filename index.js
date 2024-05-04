25be2ccda771785c537c7deac019b7794d3e37dc
valeriasolorza <v.solorza.343@gmail.comcd>
2024-05-04 11:18:25 -0700

    remaining stub routes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello world!, goodbye cruel world')
 })
 
 app.get('*', (req, res) => {
