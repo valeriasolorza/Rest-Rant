a32204f891e23efc99f9db72daf0521f636552dc
valeriasolorza <v.solorza.343@gmail.comcd>
2024-05-09 22:59:33 -0700

    adding default view

---
+++ views/.default.jsx
@@ -0,0 +1,19 @@
+const React = require('react')
+
+function Def (html) {}
+
+module.exports = Def
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+  }
+  
