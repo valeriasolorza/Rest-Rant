d28aca07c59689da6657ed15a24c28950705421b
valeriasolorza <v.solorza.343@gmail.comcd>
2024-05-20 18:38:24 -0700

    show route connected?

--- controllers/places.js
+++ controllers/places.js
@@ -28,3 +28,18 @@ router.get('/new', (req, res) => {
 })
 
 module.exports = router
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+res.render('places/show', {place: places[id]})
+    }
+  })
+  
+  
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,20 +2,21 @@ const React = require("react");
 const Def = require("../default");
 
 function index(data) {
-  let placesFormatted = data.places.map((place) => {
+  let placesFormatted = data.places.map((place, index) => {
     return (
       <div className="col-sm-6">
         <h2>
-          <a href={`/places/${place.id}`}>{place.name}</a>
+          <a href={`/places/${index}`}>{place.name}</a>
         </h2>
         <p className="text-center">{place.cuisines}</p>
-        <img src={place.pic} alt={place.name}></img>
+        <img src={place.pic} alt={place.name} />
         <p className="text-center">
           Located in {place.city}, {place.state}
         </p>
       </div>
     );
   });
+
   return (
     <Def>
       <main>
