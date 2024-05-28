5071813242773d46f58c4a15cace328d2f23d447
valeriasolorza <v.solorza.343@gmail.comcd>
2024-05-27 19:17:02 -0700

    Completed rest of delete and edit forms in places.js

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form(data) {
     <Def>
       <main>
         <h1>Edit Place</h1>
-        <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+        <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
           <div className="row">
             <div className="form-group col-sm-6">
               <label htmlFor="name">Place Name</label>
@@ -58,6 +58,16 @@ function edit_form(data) {
               required
             />
           </div>
+          <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded</label>
+            <input
+              className="form-control"
+              id="founded"
+              name="founded"
+              value={data.place.founded}
+            />
+          </div>
+
           <input
             className="btn btn-primary"
             type="submit"
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,26 +1,31 @@
-const React = require('react');
-const Def = require('../default');
+const React = require("react");
+const Def = require("../default");
 
 function show(data) {
-  let comments = (
-    <h3 className="inactive">
-      No comments yet!
-    </h3>
-  )
+  let comments = <h3 className="inactive">No comments yet!</h3>;
   let rating = (
-    <h3 className="inactive"
-      style={{ color: 'darkgray' }}
-    >
+    <h3 className="inactive" style={{ color: "darkgray" }}>
       Not yet rated!
     </h3>
-  )
+  );
 
+  //RATING
   if (data.place.comments.length) {
-    comments = data.place.comments.map(c => {
-      // ! Im at here 
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars;
+    }, 0);
+    let averageRating = Math.round(sumRatings / data.place.comments.length);
+    let stars = "";
+    for (let i = 0; i < averageRating; i++) {
+      stars += "⭐";
+    }
+    rating = <h3>{stars} stars</h3>;
+
+    //COMMENTS
+    comments = data.place.comments.map((c) => {
       return (
         <div className="border" key={c._id}>
-          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+          <h2 className="rant">{c.rant ? "Rant! 😡" : "Rave! 😻"}</h2>
           <h4>{c.content}</h4>
           <h3>
             <strong>- {c.author}</strong>
@@ -31,11 +36,11 @@ function show(data) {
     });
   }
 
+  //SHOW
   return (
     <Def>
       <main className="show-main">
-
-        <section className='main-left-section'>
+        <section className="main-left-section">
           <div>
             <img src={data.place.pic} alt={data.place.name} />
             <h3>
@@ -44,8 +49,7 @@ function show(data) {
           </div>
         </section>
 
-     
-        <section className='main-right-section'>
+        <section className="main-right-section">
           <div>
             <h1>{data.place.name}</h1>
             <div>
@@ -54,25 +58,21 @@ function show(data) {
             </div>
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
-            <h4
-              style={{ color: 'darkgray' }}
-            >
+            <h4 style={{ color: "darkgray" }}>
               Serving
               {data.place.cuisines}
             </h4>
           </div>
 
-
-          <div className='show-edit-delete-buttons'>
-            <button className="btn btn-warning"
-              style={{ marginRight: '10px' }}
-            >
-              <a href={`/places/${data.place._id}/edit`}>
-                Edit
-              </a>
+          <div className="show-edit-delete-buttons">
+            <button className="btn btn-warning" style={{ marginRight: "10px" }}>
+              <a href={`/places/${data.place._id}/edit`}>Edit</a>
             </button>
 
-            <form method="POST" action={`/places/${data.place._id}?_method=DELETE`}>
+            <form
+              method="POST"
+              action={`/places/${data.place._id}?_method=DELETE`}
+            >
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
@@ -82,31 +82,25 @@ function show(data) {
 
         <section className="row main-mid-section">
           <div>
-            <h2
-              style={{ color: 'cornflowerblue' }}
-            >
-              Comments
-            </h2>
-            <div className='section-mid-comments'>
-            {comments}
-            </div>
+            <h2 style={{ color: "cornflowerblue" }}>Comments</h2>
+            <div className="section-mid-comments">{comments}</div>
           </div>
         </section>
 
         <section className="row main-bottom-section">
-          <h2
-            style={{ color: 'cornflowerblue' }}
-          >
-            Leave a Review
-          </h2>
+          <h2 style={{ color: "cornflowerblue" }}>Leave a Review</h2>
 
-          <form id="comment-form" action={`/places/${data.place._id}/comment`} method="POST">
+          <form
+            id="comment-form"
+            action={`/places/${data.place._id}/comment`}
+            method="POST"
+          >
             <label htmlFor="author"> Your Name: </label>
             <div
               style={{
-                display: 'flex',
-                flexFlow: 'wrap row',
-                width: '100%'
+                display: "flex",
+                flexFlow: "wrap row",
+                width: "100%",
               }}
             >
               <input
@@ -114,30 +108,43 @@ function show(data) {
                 name="author"
                 id="author"
                 className="form-control"
-                style={{ backgroundColor: 'white', color: 'black', borderRadius: '6px', margin: '10px', }}
-              >
-              </input>
+                style={{
+                  backgroundColor: "white",
+                  color: "black",
+                  borderRadius: "6px",
+                  margin: "10px",
+                }}
+              ></input>
 
-              <div style={{ flexGrow: '1' }}>
+              <div style={{ flexGrow: "1" }}>
                 <label htmlFor="content"> Comment: </label>
                 <input
                   type="textarea"
                   name="content"
                   id="content"
                   className="form-control"
-                  style={{ backgroundColor: 'white', color: 'black', borderRadius: '6px', margin: '10px', }}
-                >
-                </input>
+                  style={{
+                    backgroundColor: "white",
+                    color: "black",
+                    borderRadius: "6px",
+                    margin: "10px",
+                  }}
+                ></input>
               </div>
 
               <br />
 
-              <div className="star-rating"
-                style={{}}
-              >
+              <div className="star-rating" style={{}}>
                 <label htmlFor="stars"> Star Rating </label>
                 <br />
-                <input type="range" name="stars" min="0" max="5" step="0.5" required></input>
+                <input
+                  type="range"
+                  name="stars"
+                  min="0"
+                  max="5"
+                  step="0.5"
+                  required
+                ></input>
                 <br />
                 <datalist id="values">
                   <option value="0" label="0"></option>
@@ -157,16 +164,15 @@ function show(data) {
             </div>
 
             <div>
-              <button
-                type="submit"
-                className="btn btn-outline-primary"
-              > Add Comment </button>
+              <button type="submit" className="btn btn-outline-primary">
+                {" "}
+                Add Comment{" "}
+              </button>
             </div>
-
           </form>
         </section>
       </main>
-    </Def >
+    </Def>
   );
 }
 
